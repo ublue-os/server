fec84934f11d726ce88d2e15ce45617381ef2d83
Benjamin Sherman <benjamin@holyarmy.org>
2025-06-10 02:54:25 +0000

    chore: enable nas ZFS install of sanoid

--- build_files/21-nas-zfs.sh
+++ build_files/21-nas-zfs.sh
@@ -2,8 +2,6 @@
 set -xeuo pipefail
 
 ### install NAS server ZFS packages
-# TODO: switch this package to ublue-os/staging COPR
-#dnf -y copr enable ublue-os/staging
-#dnf -y install sanoid
-#dnf -y copr disable ublue-os/staging
-echo "TODO: enable here after adding ublue-os/akmods ZFS install"
+dnf -y copr enable ublue-os/staging
+dnf -y install sanoid
+dnf -y copr disable ublue-os/staging
