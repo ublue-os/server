351352caeea7262402f1189414423397887762c8
m2 <69128853+m2Giles@users.noreply.github.com>
2025-06-16 18:28:19 +0000

    use ublue-os-libvirt-workarounds package
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- build_files/21-nas-zfs.sh
+++ build_files/21-nas-zfs.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-# TODO: Enable once building in copr
 ### install NAS server ZFS packages
 # dnf -y copr enable ublue-os/staging
 # dnf -y install sanoid
--- build_files/30-virt-packages.sh
+++ build_files/30-virt-packages.sh
@@ -2,12 +2,14 @@
 set -xeuo pipefail
 
 ### install virt server packages
+dnf -y copr enable ublue-os/packages
 
 ### server hci packages which are mostly what we added in ucore-hci
 dnf -y install --setopt=install_weak_deps=False \
     cockpit-machines \
     libvirt-client \
     libvirt-daemon-kvm \
+    ublue-os-libvirt-workarounds \
     virt-install
 
 ### sysusers.d for libvirtdbus
@@ -15,26 +17,4 @@ cat >/usr/lib/sysusers.d/libvirt-dbus.conf <<'EOF'
 u libvirtdbus - 'Libvirt D-Bus bridge' - -
 EOF
 
-### Libvirt SELinux Labeling Workarounds
-cat >/usr/lib/systemd/system/libvirt-restorecon.service <<'EOF'
-[Unit]
-Description=Workaround to relabel libvirt files and directories
-ConditionPathIsDirectory=/var/lib/libvirt/
-ConditionPathIsDirectory=/var/log/libvirt/
-After=local-fs.target systemd-tmpfiles-setup.service
-
-[Service]
-Type=oneshot
-ExecStart=-/usr/sbin/restorecon -R /var/log/libvirt/
-ExecStart=-/usr/sbin/restorecon -R /var/lib/libvirt/
-RemainAfterExit=yes
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-cat >/usr/lib/systemd/system-preset/80-libvirt-restorecon.preset <<'EOF'
-enable libvirt-restorecon.service
-EOF
-
-systemctl preset libvirt-restorecon.service
+dnf -y copr disable ublue-os/packages
