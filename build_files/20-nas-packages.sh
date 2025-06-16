b9045f9ca5f41f82e6069ee3346ceb0c3a6f144e
Benjamin Sherman <benjamin@holyarmy.org>
2025-06-16 12:19:19 -0500

    feat: re-add sanoid, add duperemove, add cosign pub key

--- build_files/20-nas-packages.sh
+++ build_files/20-nas-packages.sh
@@ -9,6 +9,7 @@ dnf -y install --setopt=install_weak_deps=False \
     NetworkManager-wifi \
     cockpit-storaged \
     distrobox \
+    duperemove \
     hdparm \
     pcp-zeroconf \
     samba \
@@ -17,14 +18,11 @@ dnf -y install --setopt=install_weak_deps=False \
     xdg-dbus-proxy \
     xdg-user-dirs
 
-# TODO: these require EPEL packages to be built for el10
-#    duperemove \
-
 dnf -y copr enable ublue-os/staging
 dnf -y install snapraid
 dnf -y copr disable ublue-os/staging
 
 ### install packages direct from github
-### NOTE: we need to get proper arch for these packages rather than hard coding
+### NOTE: ARM support will require use of proper arch rather than hard coding
 /ctx/github-release-install.sh rclone/rclone "linux-amd64"
 /ctx/github-release-install.sh trapexit/mergerfs "el${RELEASE}.x86_64"
