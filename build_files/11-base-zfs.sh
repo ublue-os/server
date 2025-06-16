2f560d03aded696bbb2d773ba40e6c3aec4da00f
Benjamin Sherman <benjamin@holyarmy.org>
2025-06-15 19:18:00 -0500

    a note for future

--- build_files/11-base-zfs.sh
+++ build_files/11-base-zfs.sh
@@ -6,6 +6,9 @@ dnf -y install \
     pv \
     /tmp/akmods-zfs-rpms/kmods/zfs/*.rpm \
     /tmp/akmods-zfs-rpms/kmods/zfs/other/zfs-dracut-*.rpm
+# NOTE: maybe we need to be careful about installing only the kmod-zfs which matches KERNEL_VR
+# eg, KERNEL_VR="$(rpm -q "kernel" --queryformat '%{VERSION}-%{RELEASE}')"
+#  dnf install -y  /tmp/akmods-zfs-rpms/kmods/zfs-kmod-*"${KERNEL_VR}"*.rpm
 
 # depmod ran automatically with zfs 2.1 but not with 2.2
 KERNEL_VRA="$(rpm -q "kernel" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
