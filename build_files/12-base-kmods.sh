bcbf710c73e31e3c6b49a898e749cecb91354df3
Benjamin Sherman <benjamin@holyarmy.org>
2025-06-19 00:14:41 -0500

    feat: add Kmods SIG kmods for CentOS kernel
    This enables the CentOS Kmods SIG repos and adds kmods and userspace
    packages which bring the installed kernels features into closer
    alignment with what we had under Fedora CoreOS.
    
    Namely, this includes drivers for:
    - btrfs (plus userspace)
    - ntfs3
    - vbox-guests
    - various network controllers
    - various storage controllers
    
    Closes: #24

---
+++ build_files/12-base-kmods.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+# enable Kmods SIG repos
+dnf -y install https://mirror.stream.centos.org/SIGs/$(rpm --eval '%{?rhel}/kmods/%{_arch}/repos-main/Packages/c/centos-repos-kmods-%{?rhel}-2.el%{?rhel}.noarch.rpm')
+dnf -y install centos-release-kmods
+dnf config-manager --set-enabled centos-kmods-rebuild
+
+# install desired kmods and utils
+dnf -y install \
+    btrfs-progs \
+    kmod-aacraid \
+    kmod-be2iscsi \
+    kmod-be2net \
+    kmod-btrfs \
+    kmod-hpsa \
+    kmod-lpfc \
+    kmod-megaraid_sas \
+    kmod-mpt3sas \
+    kmod-mptsas \
+    kmod-mptspi \
+    kmod-ntfs3 \
+    kmod-qla2xxx \
+    kmod-vbox-guest-additions
+
+# typically we disable extra repos, but like CRB and EPEL
+# this repo is from CentOS so we leave it enabled
