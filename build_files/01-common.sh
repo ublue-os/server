0bfb238ba14d25c80b50ab96363cc608373c5bec
m2 <69128853+m2Giles@users.noreply.github.com>
2025-06-19 13:34:56 +0000

    Consistent style
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- build_files/01-common.sh
+++ build_files/01-common.sh
@@ -5,29 +5,39 @@
 
 set -xeuo pipefail
 
-# /* See https://github.com/CentOS/centos-bootc/issues/191 */
+# /* 
+# See https://github.com/CentOS/centos-bootc/issues/191
+# */
 mkdir -m 0700 -p /var/roothome
 
-# /* make /usr/local and /opt writable */
+# /*
+# make /usr/local and /opt writable
+# */
 mkdir -p /var/{opt,usrlocal}
 rm -rf /opt /usr/local
 ln -sf var/opt /opt
 ln -sf ../var/usrlocal /usr/local
 
-# /* remove subscription manager */
+# /*
+# remove subscription manager
+# */
 dnf -y remove \
   libdnf-plugin-subscription-manager \
   python3-subscription-manager-rhsm \
   subscription-manager \
   subscription-manager-rhsm-certificates
 
-# /* enable CRB, EPEL and other repos */
+# /*
+# enable CRB, EPEL and other repos
+# */
 dnf config-manager --set-enabled crb
 dnf -y install epel-release
 dnf -y upgrade epel-release
 
-# /* packages which are more or less what we'd find in CoreOS
-# other than ignition, coreos-installer, moby-engine, etc */
+# /*
+# packages which are more or less what we'd find in CoreOS
+# other than ignition, coreos-installer, moby-engine, etc
+# */
 dnf -y install --setopt=install_weak_deps=False \
   audit \
   git-core \
@@ -38,13 +48,17 @@ dnf -y install --setopt=install_weak_deps=False \
   rsync \
   ssh-key-dir
 
-# /* Configure Updates */
+# /*
+# Configure Updates
+# */
 sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/bootc update --quiet|' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
 sed -i 's|^OnUnitInactiveSec=.*|OnUnitInactiveSec=7d\nPersistent=true|' /usr/lib/systemd/system/bootc-fetch-apply-updates.timer
 sed -i 's|#AutomaticUpdatePolicy.*|AutomaticUpdatePolicy=stage|' /etc/rpm-ostreed.conf
 sed -i 's|#LockLayering.*|LockLayering=true|' /etc/rpm-ostreed.conf
 
-# /* Zram Generator */
+# /*
+# Zram Generator
+# */
 cat >/usr/lib/systemd/zram-generator.conf<<'EOF'
 [zram0]
 zram-size = min(ram, 8192)
