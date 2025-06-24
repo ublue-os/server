71c4341bcaa4944ab2a6b0074c41bd7eee26006b
Benjamin Sherman <benjamin@holyarmy.org>
2025-06-23 22:36:03 -0500

    chore: remove Cockpit ZFS plugin
    Relates: #63

--- build_files/13-base-configurations.sh
+++ build_files/13-base-configurations.sh
@@ -102,28 +102,3 @@ EOF
 cat >/usr/lib/tmpfiles.d/cayo-duperemove.conf <<'EOF'
 d /var/lib/duperemove - - - -
 EOF
-
-# /*
-### Cockpit Plugin
-# */
-
-# /*
-# add cockpit plugin for ZFS management
-# */
-curl --fail --retry 15 --retry-all-errors -sSL -o /tmp/cockpit-zfs-manager-api.json \
-    "https://api.github.com/repos/45Drives/cockpit-zfs-manager/releases/latest"
-CZM_TGZ_URL=$(jq -r .tarball_url /tmp/cockpit-zfs-manager-api.json)
-curl --fail --retry 15 --retry-all-errors -sSL -o /tmp/cockpit-zfs-manager.tar.gz "${CZM_TGZ_URL}"
-
-mkdir -p /tmp/cockpit-zfs-manager
-tar -zxvf /tmp/cockpit-zfs-manager.tar.gz -C /tmp/cockpit-zfs-manager --strip-components=1
-cp -a /tmp/cockpit-zfs-manager/polkit-1/actions/. /usr/share/polkit-1/actions/
-cp -a /tmp/cockpit-zfs-manager/polkit-1/rules.d/. /usr/share/polkit-1/rules.d/
-mv /tmp/cockpit-zfs-manager/zfs /usr/share/cockpit
-
-curl --fail --retry 15 --retry-all-errors -sSL -o /tmp/cockpit-zfs-manager-font-fix.sh \
-    https://raw.githubusercontent.com/45Drives/scripts/refs/heads/main/cockpit_font_fix/fix-cockpit.sh
-chmod +x /tmp/cockpit-zfs-manager-font-fix.sh
-/tmp/cockpit-zfs-manager-font-fix.sh
-
-rm -rf /tmp/cockpit-zfs-manager*
